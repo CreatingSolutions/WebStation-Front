0f5be9f7542059f0c1c6d5292e9504c57be2ccc9
Mathieu De Baerdemaeker <mathieu.debaerdemaeker@gmail.com>
2019-03-23 19:44:03 +0100

    fix: mock

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,8 +6,7 @@ import {
 import { Observable, of } from 'rxjs';
 import { LoadingService } from './loading.service';
 import {environment} from '../../environments/environment';
-import {Flat, User, Lift, Stuff, School, Cart} from '../store/models';
-import { timeout } from 'q';
+import {Flat, Lift, Stuff, School, Cart} from '../store/models';
 
 @Injectable()
 export class ApiService {
@@ -64,7 +63,39 @@ export class ApiService {
   }
 
   public getAllSchool(): Observable<School[]> {
-    return this.httpClient.get<School[]>(`${environment.apiUrl}/ecole/pack`);
+    return of<School[]>(
+      [
+        {
+          packId: 1,
+          period: {
+            periodId: 1,
+            startDate: new Date(),
+            endDate: new Date()
+          },
+          tickets: {
+            MORNING: 3,
+            MOON: 4,
+            AFTERNOON: 4,
+            EVENING: 3,
+            ALL: 10
+          },
+          price: 50,
+          available: true
+        },
+        {
+          packId: 2,
+          period: {
+            periodId: 2,
+            startDate: new Date(),
+            endDate: new Date()
+          },
+          tickets: {
+            MORNING: 3
+          },
+          price: 10,
+          available: true
+        }
+      ]);
   }
 
   public login(email: string, password: string): Observable<any> {
@@ -80,7 +111,51 @@ export class ApiService {
   }
 
   public getCartOf(): Observable<any> {
-    return this.httpClient.get<any>(`${environment.apiUrl}/cart`);
+    return of<Cart>(
+      {
+        cartId: 48,
+        id: 48,
+        flats: [
+          {
+            flatId: 1,
+            title: 'Appartement situé plein sud',
+            description: 'L\'appartement situé dans un petit coin de paradis avec cristiano ronaldo criant Siiiiiiiiiii',
+            price: 200
+          },
+          {
+            flatId: 2,
+            title: 'Appartement situé plein sud',
+            description: 'L\'appartement situé dans un petit coin de paradis avec cristiano ronaldo criant Siiiiiiiiiii',
+            price: 400.2
+          }
+        ],
+        totalFlatPrice: 200,
+        lifts: [
+          {
+            liftId: 1,
+            title: 'Forfait 1/2 J',
+            description: 'Forfait jeune',
+            taked: 3,
+            insurance: false,
+            price: 80
+          }
+        ],
+        totalLiftPrice: 200,
+        stuffs: [
+          {
+            stuffId: 1,
+            title: 'Skis alpins avec bâtons et chaussuuuuuures pour adultes',
+            description: 'Vendu par ToutSchuss',
+            taked: 3,
+            price: 80
+          }
+        ],
+        totalStuffPrice: 200,
+        totalPrice: 600,
+        validate: false,
+        paid: false
+      }
+    );
   }
 
   public deleteCart(): Observable<Cart> {
